705c1df5776c96fd80324d2c7e42fa8b15a531c9
alex.kopachov <alex.kopachov@gmail.com>
2023-03-15 13:58:05 +0100

    Added 3 builds: with badbt, withbt but without icon, without badbt at all

--- build.ps1
+++ build.ps1
@@ -11,13 +11,13 @@ $build_commands = @(
         Name = "Official Dev \ Unleashed";
         FbtSwitch = "od";
         FirmwarePath = "flipperzero-firmware_official_dev";
-        ArtifactName = "totp_official-dev_unleashed_fw.fap"
+        ArtifactName = "totp_official-dev_unleashed_fw{FEATURES_SUFFIX}.fap"
     }
     [PSCustomObject]@{
         Name = "Official Stable";
         FbtSwitch = "os";
         FirmwarePath = "flipperzero-firmware_official_stable";
-        ArtifactName = "totp_official-stable_fw.fap"
+        ArtifactName = "totp_official-stable_fw{FEATURES_SUFFIX}.fap"
     }
 )
 
@@ -29,20 +29,40 @@ if (!(Test-Path -PathType Container "build")) {
     Remove-Item "build\*" -Recurse -Force
 }
 
-foreach ($build_command in $build_commands) {
-    Write-Host "Building $($build_command.Name)"
-    $build_path = Join-Path -Path $build_command.FirmwarePath -ChildPath "build"
-    if (Test-Path -PathType Container $build_path) {
-        Remove-Item "$build_path\*" -Recurse -Force
+function Build-Run() {
+    param (
+        [string]$FeaturesSuffix
+    )
+
+    foreach ($build_command in $build_commands) {
+        Write-Host "Building $($build_command.Name)"
+        $build_path = Join-Path -Path $build_command.FirmwarePath -ChildPath "build"
+        if (Test-Path -PathType Container $build_path) {
+            Remove-Item "$build_path\*" -Recurse -Force
+        }
+
+        ./fbt $build_command.FbtSwitch COMPACT=1 DEBUG=0 VERBOSE=0 fap_totp
+
+        $latest_dir = Get-LatestDirectory -Path $build_path
+        $build_output_artifact = "build\$($build_command.ArtifactName -replace '{FEATURES_SUFFIX}',$FeaturesSuffix)"
+        Copy-Item "$build_path\$latest_dir\.extapps\totp.fap" -Destination $build_output_artifact
+
+        Write-Host "Artifacts for $($build_command.Name) stored at $build_output_artifact"
     }
-
-    ./fbt $build_command.FbtSwitch COMPACT=1 DEBUG=0 VERBOSE=0 fap_totp
-
-    $latest_dir = Get-LatestDirectory -Path $build_path
-    $build_output_artifact = "build\$($build_command.ArtifactName)"
-    Copy-Item "$build_path\$latest_dir\.extapps\totp.fap" -Destination $build_output_artifact
-
-    Write-Host "Artifacts for $($build_command.Name) stored at $build_output_artifact"
 }
+
+Write-Information 'Building with all the features enables'
+./features-config.ps1 -enable TOTP_BADBT_TYPE_ENABLED,TOTP_BADBT_TYPE_ICON_ENABLED
+Build-Run -FeaturesSuffix ''
+
+Write-Information 'Building with BadBT but without BadBT icon'
+./features-config.ps1 -disable TOTP_BADBT_TYPE_ICON_ENABLED
+Build-Run -FeaturesSuffix '_badbt-wo-icon'
+
+Write-Information 'Building without BadBT'
+./features-config.ps1 -disable TOTP_BADBT_TYPE_ENABLED,TOTP_BADBT_TYPE_ICON_ENABLED
+Build-Run -FeaturesSuffix '_no-badbt'
+
+./features-config.ps1 -enable TOTP_BADBT_TYPE_ENABLED,TOTP_BADBT_TYPE_ICON_ENABLED
 
 Pop-Location
